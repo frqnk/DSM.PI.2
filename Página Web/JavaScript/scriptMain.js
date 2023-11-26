27c6fa274e9488e96b19be55521dfebe57ee10bd
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2023-11-26 19:22:26 -0300

    Pequenas mudanças

---
+++ Página Web/JavaScript/scriptMain.js
@@ -0,0 +1,17 @@
+// Relógio
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    
+    const timeString = `${hours}:${minutes}`;
+
+    const clockElement = document.getElementById('clock');
+    clockElement.textContent = timeString;
+}
+
+// Atualiza o relógio a cada segundo
+setInterval(updateClock, 1000);
+
+// Atualiza o relógio imediatamente ao carregar a página
+updateClock();
